71a8191233f9209d6134689f9b892ea2e2126e9d
CulperCypher <blakeuhill@gmail.com>
2025-10-04 18:04:02 -0400

    Asset Card Layout

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ export default function App() {
           </div>
 
           {/* Asset Selection */}
-          <div className="flex overflow-x-auto md:grid md:grid-cols-4 gap-4 w-full max-w-5xl mx-auto snap-x snap-mandatory scrollbar-hide pb-4 px-4 md:px-0">
+          <div className="flex overflow-x-auto md:grid md:grid-cols-4 gap-4 mx-auto snap-x snap-mandatory scrollbar-hide pb-4 px-4 md:px-8">
             {/* AVAX Card */}
             <button
               onClick={() => handleAssetChange('avax')}
